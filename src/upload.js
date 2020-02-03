4a38a04fd00a94c5555d71106210b1c4b05437cd
Vaidas <shibalba1@gmail.com>
2020-02-03 21:14:10 +0100

    update upload script

--- src/upload.js
+++ src/upload.js
@@ -1,20 +1,24 @@
 const AWS = require('aws-sdk');
 const fs = require('fs');
 const path = require('path');
-const secrets = require('secrets');
+const {
+  ACCESS_KEY_ID,
+  SECRET_ACCESS_KEY,
+  BUCKET_NAME
+} = require('./secrets');
 
 const s3 = new AWS.S3({
   apiVersion: '2006-03-01',
   sslEnabled: true,
-  secrets.ACCESS_KEY_ID,
-  secrets.SECRET_ACCESS_KEY
+  accessKeyId: ACCESS_KEY_ID,
+  secretAccessKey: SECRET_ACCESS_KEY
 });
 
 const uploadFile = (filePath, bucketFileName) => {
   let fileContents = fs.readFileSync(filePath);
 
   s3.upload({
-    Bucket: secrets.BUCKET_NAME,
+    Bucket: BUCKET_NAME,
     Key: bucketFileName,
     Body: fileContents,
     ACL:'public-read'
