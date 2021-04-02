f64e18f1e2186aa98e9c565a012b15732f8244b0
Gyvastis <shibalba1@gmail.com>
2021-04-02 19:18:12 +0200

    add race condition for timeout

--- src/index.js
+++ src/index.js
@@ -18,6 +18,11 @@ const outputFile = './output/output.json';
 const concurrency = 30;
 const timeout = 8000;
 
+const timeoutPromise = () => new Promise((resolve, reject) => {
+  const timeoutWhen = timeout * 1.5;
+  setTimeout(() => reject(new Error(`Rejected - timeout of ${timeoutWhen}ms exceeded`)), timeoutWhen);
+});
+
 function shuffle(a) {
   for (let i = a.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
@@ -90,18 +95,19 @@ const writeOutputAppend = (dataItem) => {
   // let proxies = await readFile('./src/proxies.json', 'utf8').then(JSON.parse)
   let proxies = await fetch('https://raw.githubusercontent.com/HandyProxy/node-proxy-scraper/master/output/output.merged.json').then(res => res.json());
   proxies = shuffle(proxies)
-  // .slice(1,10)
+  .slice(1,10)
 
   const startedAt = new Date().getTime();
   let processed = 0;
   fs.writeFileSync(outputFile, "[\n")
 
-  Promise.map(proxies, proxy => {
-    return checkProxy(proxy.ip, proxy.port, proxy.protocol)
+  await Promise.map(proxies, proxy => {
+    return Promise.race([checkProxy(proxy.ip, proxy.port, proxy.protocol), timeoutPromise()])
       .then(data => ({
         ...proxy,
         ...data,
         hidesIp: data.realIp !== myIp,
+        realIp: myIp,
       }))
       .then(data => {
         console.log(`${++processed}/${proxies.length} ${JSON.stringify(data)}`)
@@ -112,8 +118,11 @@ const writeOutputAppend = (dataItem) => {
         console.log(`${++processed}/${proxies.length} ${err.name}: ${proxy.protocol}://${proxy.ip}:${proxy.port}. ${err.message}`);
       });
   }, { concurrency })
-  .then(async () => {
-    await readFile(outputFile, 'utf8').then(json => { fs.writeFileSync(outputFile, json.replace(/[^}]+$/g, '')+"\n]") } );
-  })
-  .then(() => console.log(new Date().getTime() - startedAt));
+  .then(() => console.log(new Date().getTime() - startedAt))
+  .catch(err => console.log(err));
+
+  await readFile(outputFile, 'utf8').then(json => { fs.writeFileSync(outputFile, json.replace(/[^}]+$/g, '')+"\n]") } );
+
+  console.log('Done');
+  process.exit(0);
 })();
