6b143f69771ffd382c86d0187a3a527ca8836264
Vaidas <vaidas.bagdonas@comsave.com>
2021-04-04 10:17:21 +0200

    default anonymity to anonymous

--- src/index.js
+++ src/index.js
@@ -108,7 +108,7 @@ const writeOutputAppend = (dataItem) => {
         ...data,
         hidesIp: data.realIp !== myIp,
         realIp: null,
-        anonymity: data.anonymity ? data.anonymity : 'anonymous'
+        anonymity: proxy.anonymity ? proxy.anonymity : 'anonymous'
       }))
       .then(data => {
         console.log(`${++processed}/${proxies.length} ${JSON.stringify(data)}`)
